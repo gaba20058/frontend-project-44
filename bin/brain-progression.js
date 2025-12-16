ba4a42bcda18fb6c7e755d9161b293436864b907
romagun <romaorlov504@gmail.com>
2025-12-16 16:08:40 +0300

    new3

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -7,7 +7,7 @@ const brainProgression=()=>{
     const {start,step,length,hiddenIndex}=generateProgressionParams();
     const progression=generateProgression(start,step,length);
     const hiddenProgression=hideElement(progression,hiddenIndex);
-    const correctAnswer=progression[hiddenIndex];
+    const correctAnswer=String(progression[hiddenIndex]);
 
     return [hiddenProgression,correctAnswer];
 };
